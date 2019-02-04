9f04b0e38ab08e4b6f68c0f3e3b385286cfcd22b
Евланов Александр Сергеевич <Evlanov.Alexandr@gtrf.local>
2019-02-04 10:48:23 +0300

    update bootstrap_mdadm.sh

--- module-1/drive-subsystem/bootstrap_mdadm.sh
+++ module-1/drive-subsystem/bootstrap_mdadm.sh
@@ -1,12 +1,12 @@
 #build raid 10
 
 build_raid() {
-	yes "" | mdadm --create --verbose /dev/md0 --level=10 --raid-devices=/dev/sd[b-e]
+yes "" | mdadm --create --verbose /dev/md0 --level=10 --raid-devices=/dev/sd{b,c,d,e}
 }
 
 #make conf
 
 save_raid_conf() {
-	bash -c "echo \"DEVICE partitions\" > /etc/mdadm/mdadm.conf"
-	bash -c "mdadm --detail --scan | awk '/ARRAY/ {print}' >> /etc/mdadm.conf"
+bash -c "echo \"DEVICE partitions\" > /etc/mdadm/mdadm.conf"
+bash -c "mdadm --detail --scan | awk '/ARRAY/ {print}' >> /etc/mdadm.conf"
 }
